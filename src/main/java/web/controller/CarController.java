f5d07ad569c508acf39ebfd4d9824083e738631d
Ten Valeriy <walera.tenlist.ru@MacBook-Air-Ten.local>
2025-04-27 01:17:30 +0900

    TEST

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,23 +1,30 @@
 package web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import web.model.Car;
 import web.service.CarService;
 
-import java.util.ResourceBundle;
+import java.util.List;
 
 @Controller
-@RequestMapping("/cars")
 public class CarController {
 
-    @GetMapping
-    public String getCars(@RequestParam(name="locale", defaultValue = "en", required = false)String locale,  ModelMap model) {
-        model.addAttribute("cars", new CarService().getAllCars());
-        ResourceBundle bundle = ResourceBundle.getBundle("language_" + locale);
-        model.addAttribute("headline", bundle.getString("headline"));
+    @Autowired
+    private CarService carService;
+
+    @GetMapping("/cars")
+    public String showCars(@RequestParam(value = "count", required = false) Integer count, Model model) {
+        List<Car> cars;
+        if (count == null) {
+            cars = carService.getAllCars();
+        } else {
+            cars = carService.getCars(count);
+        }
+        model.addAttribute("cars", cars);
         return "cars";
     }
 }
--- src/main/java/web/service/CarService.java
+++ src/main/java/web/service/CarService.java
@@ -1,26 +1,33 @@
 package web.service;
 
+import org.springframework.stereotype.Service;
 import web.model.Car;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class CarService {
-    private List<Car> cars = new ArrayList<>();
+    private List<Car> cars;
+
+    public CarService() {
+        cars = new ArrayList<>();
+        cars.add(new Car("Kia", "White", 777));
+        cars.add(new Car("Lotus", "Black", 171));
+        cars.add(new Car("Mercedes", "Grey", 234));
+        cars.add(new Car("BMW", "Blue", 456));
+        cars.add(new Car("Daewoo", "Red", 456));
+    }
 
     public List<Car> getAllCars() {
-        cars.add(new Car("RangeRover", "White", 777));
-        cars.add(new Car("Mercedes", "Black", 171));
-        cars.add(new Car("BMW", "Grey", 234));
-        cars.add(new Car("Porsche", "Красный", 456));
-        return  cars;
+        return cars;
+    }
 
-        public List<Car> getCars(int count) {
-            if (count >= 5 || count < 1) {
+        public List<Car> getCars ( int count){
+            if (count >= cars.size()) {
                 return cars;
+            } else {
+                return cars.subList(0, count);
             }
-            return cars.subList(0, count);
-        }
-
     }
 }
